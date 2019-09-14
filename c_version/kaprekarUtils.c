dab53b5df8eec3c690b498f1e8ecebd892d55d2b
Victoria Nelson <nelson.victoria@huskers.unl.edu>
2019-09-13 23:04:39 -0500

    Fixed all the segmentation errors
    Fixed all the segmentation errors

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -5,30 +5,35 @@
 /**
  * <p>This function tests whether the given input <code>n</code>
  * is a Kaprekar Number or not.  A Kaprekar Number is a non-negative
- * integer n whose square can be split into two positive integers 
- * whose sum is equal to the original number.  For the purposes of 
+ * integer n whose square can be split into two positive integers
+ * whose sum is equal to the original number.  For the purposes of
  * this method, we restrict our consideration to base-10 representations.</p>
- * 
- * For example, 45 is a Kaprekar number, because 
- *   45<sup>2</sup> = 2025 
- * and 
- *   20 + 25 = 45. 
- * 
- * <p>See Wikipedia's  
+ *
+ * For example, 45 is a Kaprekar number, because
+ *   45<sup>2</sup> = 2025
+ * and
+ *   20 + 25 = 45.
+ *
+ * <p>See Wikipedia's
  * <a href="https://en.wikipedia.org/wiki/Kaprekar_number">Kaprekar Number</a>
  * article for more information.</p>
- * 
+ *
  * @param n
- * @return <code>true</code> if <code>n</code> is a Kaprekar Number, 
+ * @return <code>true</code> if <code>n</code> is a Kaprekar Number,
  * <code>false</code> otherwise.
  */
-int isKaprekar(int n) {
+
+int main(int argc, char const *argv[]) {
+
+int isKaprekar(int n);
+
+int i, n ;
 
   if(n < 1) {
-    return false;
+    return 0;
   }
 
-  int i;
+
   //compute the square
   long square = n * (long) n;
   //compute the number of digits of the square
@@ -37,9 +42,9 @@ int isKaprekar(int n) {
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+  for(i=1; i<= numDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
@@ -48,10 +53,10 @@ int isKaprekar(int n) {
     //test if the split makes a Kaprekar number
     //the second part cannot be zero and if
     //the sum of the two parts is equal to n, then it is
-    if(second > 0 && n = first + second) {
+    if(second > 0 && n == first + second) {
       return 1;
     }
   }
   return 0;
-  
+
 }
